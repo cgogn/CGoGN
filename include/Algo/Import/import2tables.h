26a2f2761ab89b1fbd903628a73a08cdcf528788
Sylvain Thery <sylvain@thery.pipo>
2011-06-10 16:37:25 +0200

    add option to merge close vertices in import (fast method)

--- include/Algo/Import/import2tables.h
+++ include/Algo/Import/import2tables.h
@@ -114,6 +114,8 @@ public:
 	bool importCTM(const std::string& filename, std::vector<std::string>& attrNames);
 
 	bool importASSIMP(const std::string& filename, std::vector<std::string>& attrNames);
+	
+	bool mergeCloseVertices();
 
 	/**
 	 * @param container container of vertex orbite
