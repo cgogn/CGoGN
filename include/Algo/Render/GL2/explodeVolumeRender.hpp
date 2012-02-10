c6dee02b69fe00d543419f14b73808e9c429d327
untereiner <lionel.untereiner@unistra.fr>
2012-02-10 16:27:33 +0100

    modification du site web en cgogn.unistra.fr dans tous les fichiers + passage du (c) a 2012 subdivisionLoop des ihm3 fonctionne a nouveau

--- include/Algo/Render/GL2/explodeVolumeRender.hpp
+++ include/Algo/Render/GL2/explodeVolumeRender.hpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
 * CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
 * version 0.1                                                                  *
-* Copyright (C) 2009-2011, IGG Team, LSIIT, University of Strasbourg           *
+* Copyright (C) 2009-2012, IGG Team, LSIIT, University of Strasbourg           *
 *                                                                              *
 * This library is free software; you can redistribute it and/or modify it      *
 * under the terms of the GNU Lesser General Public License as published by the *
@@ -17,7 +17,7 @@
 * along with this library; if not, write to the Free Software Foundation,      *
 * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
 *                                                                              *
-* Web site: http://cgogn.u-strasbg.fr/                                         *
+* Web site: http://cgogn.unistra.fr/                                           *
 * Contact information: cgogn@unistra.fr                                        *
 *                                                                              *
 *******************************************************************************/
