1dad1ea8d887e47af0bee47663a287ba7950c5f5
Sylvain Thery <sylvain@thery.pipo>
2012-05-25 14:22:52 +0200

    debug minor pb in copyFrom(): tested OK

--- src/Container/attributeContainer.cpp
+++ src/Container/attributeContainer.cpp
@@ -728,7 +728,7 @@ void  AttributeContainer::copyFrom(const AttributeContainer& cont)
 		{
 			AttributeMultiVectorGen* ptr = cont.m_tableAttribs[i]->new_obj();
 			ptr->setName(cont.m_tableAttribs[i]->getName());
-			ptr->setOrbit(cont.m_tableAttribs[i]->getIndex());
+			ptr->setOrbit(cont.m_tableAttribs[i]->getOrbit());
 			ptr->setIndex(m_tableAttribs.size());
 			ptr->setNbBlocks(cont.m_tableAttribs[i]->getNbBlocks());
 			ptr->copy(cont.m_tableAttribs[i]);
