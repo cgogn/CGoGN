acac17cee08a49f4080dc4d83748f47cbd32db54
David Cazier <cazier@caroll1.u-strasbg.fr>
2012-10-04 17:50:19 +0200

    -

--- include/Geometry/vector_gen.hpp
+++ include/Geometry/vector_gen.hpp
@@ -1,28 +1,4 @@
 /*******************************************************************************
-<<<<<<< HEAD
- * CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
- * version 0.1                                                                  *
- * Copyright (C) 2009-2011, IGG Team, LSIIT, University of Strasbourg           *
- *                                                                              *
- * This library is free software; you can redistribute it and/or modify it      *
- * under the terms of the GNU Lesser General Public License as published by the *
- * Free Software Foundation; either version 2.1 of the License, or (at your     *
- * option) any later version.                                                   *
- *                                                                              *
- * This library is distributed in the hope that it will be useful, but WITHOUT  *
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
- * for more details.                                                            *
- *                                                                              *
- * You should have received a copy of the GNU Lesser General Public License     *
- * along with this library; if not, write to the Free Software Foundation,      *
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
- *                                                                              *
- * Web site: http://cgogn.u-strasbg.fr/                                         *
- * Contact information: cgogn@unistra.fr                                        *
- *                                                                              *
- *******************************************************************************/
-=======
 * CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
 * version 0.1                                                                  *
 * Copyright (C) 2009-2012, IGG Team, LSIIT, University of Strasbourg           *
@@ -45,7 +21,6 @@
 * Contact information: cgogn@unistra.fr                                        *
 *                                                                              *
 *******************************************************************************/
->>>>>>> 69653d4f2ddc874cfdf5765126462ba58105052e
 
 #include <sstream>
 #include <cmath>
