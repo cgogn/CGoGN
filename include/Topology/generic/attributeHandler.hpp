f0c76ffaeaccd63cffa8849dcac895bbab84ec3a
Sylvain Thery <sylvain@thery.pipo>
2012-11-28 13:52:43 +0100

    restore EMBNULL testing

--- include/Topology/generic/attributeHandler.hpp
+++ include/Topology/generic/attributeHandler.hpp
@@ -137,8 +137,8 @@ inline T& AttributeHandler<T, ORBIT>::operator[](Dart d)
 	assert(valid || !"Invalid AttributeHandler") ;
 	unsigned int a = m_map->getEmbedding<ORBIT>(d) ;
 
-//	if (a == EMBNULL)
-//		a = m_map->setOrbitEmbeddingOnNewCell<ORBIT>(d) ;
+	if (a == EMBNULL)
+		a = m_map->setOrbitEmbeddingOnNewCell<ORBIT>(d) ;
 
 	return m_attrib->operator[](a) ;
 }
