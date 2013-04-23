5d7f0623fac9103ffdc4fd05b44b91d4fe7dd0f0
Sylvain Thery <thery@unistra.fr>
2013-04-23 14:43:05 +0200

    add volume exporting (NAS only)

---
+++ include/Algo/Export/exportVol.h
@@ -0,0 +1,101 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2012, IGG Team, LSIIT, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef __EXPORT_VOL_H__
+#define __EXPORT_VOL_H__
+
+#include "Topology/generic/attributeHandler.h"
+#include <stdint.h>
+
+namespace CGoGN
+{
+
+namespace Algo
+{
+
+namespace Volume
+{
+
+namespace Export
+{
+
+/**
+* export the map into a .nas (nastran file)
+* @param the_map map to be exported
+* @param position the position container
+* @param filename filename of ply file
+* @param binary write in binary mode
+* @return true
+*/
+template <typename PFP>
+bool exportNAS(typename PFP::MAP& map, const VertexAttribute<typename PFP::VEC3>& position, const char* filename) ;
+
+
+/**
+* export the map into a .msh (gmesh file)
+* @param the_map map to be exported
+* @param position the position container
+* @param filename filename of ply file
+* @param binary write in binary mode
+* @return true
+*/
+template <typename PFP>
+bool exportMSH(typename PFP::MAP& map, const VertexAttribute<typename PFP::VEC3>& position, const char* filename) ;
+
+
+/**
+* export the map into a .tet file
+* @param the_map map to be exported
+* @param position the position container
+* @param filename filename of ply file
+* @param binary write in binary mode
+* @return true
+*/
+template <typename PFP>
+bool exportTet(typename PFP::MAP& map, const VertexAttribute<typename PFP::VEC3>& position, const char* filename) ;
+
+
+/**
+* export the map into a .node/.ele file pair
+* @param the_map map to be exported
+* @param position the position container
+* @param filename filename of ply file
+* @param binary write in binary mode
+* @return true
+*/
+template <typename PFP>
+bool exportNodeEle(typename PFP::MAP& map, const VertexAttribute<typename PFP::VEC3>& position, const char* filename) ;
+
+
+} // namespace Export
+
+}
+
+} // namespace Algo
+
+} // namespace CGoGN
+
+#include "Algo/Export/export.hpp"
+
+#endif
