fd9a5728c0f6315588af585f983772e67cff5d17
untereiner <lionel.untereiner@unistra.fr>
2013-12-12 14:20:25 +0100

    [CGoGN] adding ShapeMatching + Linear Shape Matching + Quadratic Shape Matching

---
+++ include/Algo/Simulation/ShapeMatching/shapeMatchingQuadratic.h
@@ -0,0 +1,91 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2013, IGG Team, ICube, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef _SHAPE_MATCHING_QUADRATIC_H_
+#define _SHAPE_MATCHING_QUADRATIC_H_
+
+namespace CGoGN
+{
+
+namespace Algo
+{
+
+namespace Surface
+{
+
+namespace Simulation
+{
+
+namespace ShapeMatching
+{
+
+template <typename PFP>
+class ShapeMatchingQuadratic : public ShapeMatching<PFP>
+{
+public:
+    typedef typename PFP::MAP MAP;
+    typedef typename PFP::VEC3 VEC3;
+    typedef typename PFP::REAL REAL;
+
+protected:
+    REAL m_beta;
+
+    // A_{qq}
+    Eigen::Matrix3f m_aqq;
+
+    Eigen::Vector9f m_qtild;
+
+    // q^{~}_{i} where q^{~} = [q_x, q_y, q_z, q^2_x, q^2_y, q^2_z, q_x q_y , q_y q_z, q_z q_x]
+    std::vector<Eigen::Vector9f> m_qtild;
+
+private:
+    void computeAqqMatrix();
+
+public:
+    ShapeMatchingQuadratic(VertexAttribute<VEC3>& position, VertexAttribute<REAL>& mass, REAL beta):
+        ShapeMatching(position, mass),
+        m_beta(beta)
+    { }
+
+    ~ShapeMatchingQuadratic()
+    { }
+
+    void initialize();
+
+    void shapeMatch();
+};
+
+} // namespace ShapeMatching
+
+} // namespace Simulation
+
+} // namespace Surface
+
+} // namespace Algo
+
+} // namespace CGoGN
+
+#include "shapeMatchingQuadratic.hpp"
+
+#endif
---
+++ include/Algo/Simulation/ShapeMatching/shapeMatchingQuadratic.hpp
@@ -0,0 +1,54 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* version 0.1                                                                  *
+* Copyright (C) 2009-2013, IGG Team, ICube, University of Strasbourg           *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+namespace CGoGN
+{
+
+namespace Algo
+{
+
+namespace Surface
+{
+
+namespace Simulation
+{
+
+template <typename PFP>
+void ShapeMatchingLinear<PFP>::initialize()
+{
+
+    VEC3 x0cm = massCenter();
+
+
+    computeAqqMatrix();
+}
+
+
+} // namespace Simulation
+
+} // namespace Surface
+
+} // namespace Algo
+
+} // namespace CGoGN
