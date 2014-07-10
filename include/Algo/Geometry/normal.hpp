7cb4217512a146f19a34cf78d673809baf61c2b8
Kenneth Vanhoey <kenneth.vanhoey@unistra.fr>
2014-07-10 14:44:39 +0200

    clamping for cos_angle function

--- include/Algo/Geometry/normal.hpp
+++ include/Algo/Geometry/normal.hpp
@@ -45,177 +45,177 @@ namespace Geometry
 template<typename PFP, typename V_ATT>
 typename V_ATT::DATA_TYPE triangleNormal(typename PFP::MAP& map, Face f, const V_ATT& position)
 {
-	typename V_ATT::DATA_TYPE N = Geom::triangleNormal(
-		position[f.dart],
-		position[map.phi1(f)],
-		position[map.phi_1(f)]
-	) ;
-	N.normalize() ;
-	return N ;
+    typename V_ATT::DATA_TYPE N = Geom::triangleNormal(
+                position[f.dart],
+            position[map.phi1(f)],
+            position[map.phi_1(f)]
+            ) ;
+    N.normalize() ;
+    return N ;
 }
 
 template<typename PFP, typename V_ATT>
 typename V_ATT::DATA_TYPE newellNormal(typename PFP::MAP& map, Face f, const V_ATT& position)
 {
-	typedef typename V_ATT::DATA_TYPE VEC3;
-	VEC3 N(0);
+    typedef typename V_ATT::DATA_TYPE VEC3;
+    VEC3 N(0);
 
-	foreach_incident2<VERTEX>(map, f, [&] (Vertex v)
-	{
-		const VEC3& P = position[v];
-		const VEC3& Q = position[map.phi1(v)];
-		N[0] += (P[1] - Q[1]) * (P[2] + Q[2]);
-		N[1] += (P[2] - Q[2]) * (P[0] + Q[0]);
-		N[2] += (P[0] - Q[0]) * (P[1] + Q[1]);
-	});
+    foreach_incident2<VERTEX>(map, f, [&] (Vertex v)
+    {
+        const VEC3& P = position[v];
+        const VEC3& Q = position[map.phi1(v)];
+        N[0] += (P[1] - Q[1]) * (P[2] + Q[2]);
+        N[1] += (P[2] - Q[2]) * (P[0] + Q[0]);
+        N[2] += (P[0] - Q[0]) * (P[1] + Q[1]);
+    });
 
-	N.normalize();
-	return N;
+    N.normalize();
+    return N;
 }
 
 template<typename PFP, typename V_ATT>
 typename V_ATT::DATA_TYPE faceNormal(typename PFP::MAP& map, Face f, const V_ATT& position)
 {
-	if(map.faceDegree(f) == 3)
-		return triangleNormal<PFP>(map, f, position) ;
-	else
-		return newellNormal<PFP>(map, f, position) ;
+    if(map.faceDegree(f) == 3)
+        return triangleNormal<PFP>(map, f, position) ;
+    else
+        return newellNormal<PFP>(map, f, position) ;
 }
 
 template<typename PFP, typename V_ATT>
 typename V_ATT::DATA_TYPE vertexNormal(typename PFP::MAP& map, Vertex v, const V_ATT& position)
 {
-	typedef typename V_ATT::DATA_TYPE VEC3 ;
+    typedef typename V_ATT::DATA_TYPE VEC3 ;
 
-	VEC3 N(0) ;
+    VEC3 N(0) ;
 
-	foreach_incident2<FACE>(map, v, [&] (Face f)
-	{
-		VEC3 n = faceNormal<PFP>(map, f, position) ;
-		if(!n.hasNan())
-		{
-			VEC3 v1 = vectorOutOfDart<PFP>(map, f.dart, position) ;
-			VEC3 v2 = vectorOutOfDart<PFP>(map, map.phi_1(f), position) ;
-			n *= convexFaceArea<PFP>(map, f, position) / (v1.norm2() * v2.norm2()) ;
-			N += n ;
-		}
-	});
+    foreach_incident2<FACE>(map, v, [&] (Face f)
+    {
+        VEC3 n = faceNormal<PFP>(map, f, position) ;
+        if(!n.hasNan())
+        {
+            VEC3 v1 = vectorOutOfDart<PFP>(map, f.dart, position) ;
+            VEC3 v2 = vectorOutOfDart<PFP>(map, map.phi_1(f), position) ;
+            n *= convexFaceArea<PFP>(map, f, position) / (v1.norm2() * v2.norm2()) ;
+            N += n ;
+        }
+    });
 
-	N.normalize() ;
-	return N ;
+    N.normalize() ;
+    return N ;
 }
 
 template<typename PFP, typename V_ATT>
 typename V_ATT::DATA_TYPE vertexBorderNormal(typename PFP::MAP& map, Vertex v, const V_ATT& position)
 {
-	assert(map.dimension() == 3);
+    assert(map.dimension() == 3);
 
-	typedef typename V_ATT::DATA_TYPE VEC3 ;
+    typedef typename V_ATT::DATA_TYPE VEC3 ;
 
-	VEC3 N(0) ;
+    VEC3 N(0) ;
 
-	std::vector<Dart> faces;
-	faces.reserve(16);
-	map.foreach_dart_of_vertex(v, [&] (Dart d) { faces.push_back(d); });
+    std::vector<Dart> faces;
+    faces.reserve(16);
+    map.foreach_dart_of_vertex(v, [&] (Dart d) { faces.push_back(d); });
 
-	CellMarker<typename PFP::MAP, FACE> f(map);
+    CellMarker<typename PFP::MAP, FACE> f(map);
 
-	for(std::vector<Dart>::iterator it = faces.begin() ; it != faces.end() ; ++it)
-	{
-		if(!f.isMarked(*it) && map.isBoundaryIncidentFace(*it))
-		{
-			f.mark(*it);
-			VEC3 n = faceNormal<PFP>(map, *it, position);
-			if(!n.hasNan())
-			{
-				VEC3 v1 = vectorOutOfDart<PFP>(map, *it, position);
-				VEC3 v2 = vectorOutOfDart<PFP>(map, map.phi_1(*it), position);
-				n *= convexFaceArea<PFP>(map, *it, position) / (v1.norm2() * v2.norm2());
-				N += n ;
-			}
-		}
-	}
+    for(std::vector<Dart>::iterator it = faces.begin() ; it != faces.end() ; ++it)
+    {
+        if(!f.isMarked(*it) && map.isBoundaryIncidentFace(*it))
+        {
+            f.mark(*it);
+            VEC3 n = faceNormal<PFP>(map, *it, position);
+            if(!n.hasNan())
+            {
+                VEC3 v1 = vectorOutOfDart<PFP>(map, *it, position);
+                VEC3 v2 = vectorOutOfDart<PFP>(map, map.phi_1(*it), position);
+                n *= convexFaceArea<PFP>(map, *it, position) / (v1.norm2() * v2.norm2());
+                N += n ;
+            }
+        }
+    }
 
-	N.normalize() ;
-	return N ;
+    N.normalize() ;
+    return N ;
 }
 
 template <typename PFP, typename V_ATT, typename F_ATT>
 void computeNormalFaces(typename PFP::MAP& map, const V_ATT& position, F_ATT& face_normal, unsigned int thread)
 {
-	if ((CGoGN::Parallel::NumberOfThreads > 1) && (thread == 0))
-	{
-		Parallel::computeNormalFaces<PFP,V_ATT,F_ATT>(map, position, face_normal);
-		return;
-	}
+    if ((CGoGN::Parallel::NumberOfThreads > 1) && (thread == 0))
+    {
+        Parallel::computeNormalFaces<PFP,V_ATT,F_ATT>(map, position, face_normal);
+        return;
+    }
 
-	foreach_cell<FACE>(map, [&] (Face f)
-	{
-		face_normal[f] = faceNormal<PFP>(map, f, position) ;
-	}, AUTO, thread);
+    foreach_cell<FACE>(map, [&] (Face f)
+    {
+        face_normal[f] = faceNormal<PFP>(map, f, position) ;
+    }, AUTO, thread);
 }
 
 template <typename PFP, typename V_ATT>
 void computeNormalVertices(typename PFP::MAP& map, const V_ATT& position, V_ATT& normal, unsigned int thread)
 {
-	if ((CGoGN::Parallel::NumberOfThreads > 1) && (thread == 0))
-	{
-		Parallel::computeNormalVertices<PFP,V_ATT>(map, position, normal);
-		return;
-	}
+    if ((CGoGN::Parallel::NumberOfThreads > 1) && (thread == 0))
+    {
+        Parallel::computeNormalVertices<PFP,V_ATT>(map, position, normal);
+        return;
+    }
 
-	foreach_cell<VERTEX>(map, [&] (Vertex v)
-	{
-		normal[v] = vertexNormal<PFP>(map, v, position) ;
-	}, FORCE_CELL_MARKING, thread);
+    foreach_cell<VERTEX>(map, [&] (Vertex v)
+    {
+        normal[v] = vertexNormal<PFP>(map, v, position) ;
+    }, FORCE_CELL_MARKING, thread);
 }
 
 template <typename PFP, typename V_ATT>
 typename PFP::REAL computeAngleBetweenNormalsOnEdge(typename PFP::MAP& map, Edge e, const V_ATT& position)
 {
-	typedef typename V_ATT::DATA_TYPE VEC3 ;
+    typedef typename V_ATT::DATA_TYPE VEC3 ;
 
-	if(map.isBoundaryEdge(e))
-		return 0 ;
+    if(map.isBoundaryEdge(e))
+        return 0 ;
 
-	Vertex v1(e.dart);
-	Vertex v2 = map.phi2(e) ;
-	const VEC3 n1 = faceNormal<PFP>(map, Face(v1), position) ;
-	const VEC3 n2 = faceNormal<PFP>(map, Face(v2), position) ;
-	VEC3 edge = position[v2] - position[v1] ;
-	edge.normalize() ;
-	typename PFP::REAL s = edge * (n1 ^ n2) ;
-	typename PFP::REAL c = n1 * n2 ;
-	typename PFP::REAL a(0) ;
+    Vertex v1(e.dart);
+    Vertex v2 = map.phi2(e) ;
+    const VEC3 n1 = faceNormal<PFP>(map, Face(v1), position) ;
+    const VEC3 n2 = faceNormal<PFP>(map, Face(v2), position) ;
+    VEC3 edge = position[v2] - position[v1] ;
+    edge.normalize() ;
+    typename PFP::REAL s = edge * (n1 ^ n2) ;
+    typename PFP::REAL c = n1 * n2 ;
+    typename PFP::REAL a(0) ;
 
-	// the following trick is useful for avoiding NaNs (due to floating point errors)
-	if (c > 0.5) a = asin(s) ;
-	else
-	{
-		if(c < -1) c = -1 ;
-		if (s >= 0) a = acos(c) ;
-		else a = -acos(c) ;
-	}
-	//	if (isnan(a))
-	if(a != a)
-		std::cerr<< "Warning : computeAngleBetweenNormalsOnEdge returns NaN on edge " << v1 << "-" << v2 << std::endl ;
+    // the following trick is useful for avoiding NaNs (due to floating point errors)
+    if (c > 0.5) a = asin(s) ;
+    else
+    {
+        if(c < -1) c = -1 ;
+        if (s >= 0) a = acos(c) ;
+        else a = -acos(c) ;
+    }
+    //	if (isnan(a))
+    if(a != a)
+        std::cerr<< "Warning : computeAngleBetweenNormalsOnEdge returns NaN on edge " << v1 << "-" << v2 << std::endl ;
 
-	return a ;
+    return a ;
 }
 
 template <typename PFP, typename V_ATT, typename E_ATT>
 void computeAnglesBetweenNormalsOnEdges(typename PFP::MAP& map, const V_ATT& position, E_ATT& angles, unsigned int thread)
 {
-	if ((CGoGN::Parallel::NumberOfThreads > 1) && (thread == 0))
-	{
-		Parallel::computeAnglesBetweenNormalsOnEdges<PFP,V_ATT,E_ATT>(map, position, angles);
-		return;
-	}
+    if ((CGoGN::Parallel::NumberOfThreads > 1) && (thread == 0))
+    {
+        Parallel::computeAnglesBetweenNormalsOnEdges<PFP,V_ATT,E_ATT>(map, position, angles);
+        return;
+    }
 
-	foreach_cell<EDGE>(map, [&] (Edge e)
-	{
-		angles[e] = computeAngleBetweenNormalsOnEdge<PFP>(map, e, position) ;
-	}, AUTO, thread);
+    foreach_cell<EDGE>(map, [&] (Edge e)
+    {
+        angles[e] = computeAngleBetweenNormalsOnEdge<PFP>(map, e, position) ;
+    }, AUTO, thread);
 }
 
 
@@ -225,28 +225,28 @@ namespace Parallel
 template <typename PFP, typename V_ATT>
 void computeNormalVertices(typename PFP::MAP& map, const V_ATT& position, V_ATT& normal)
 {
-	CGoGN::Parallel::foreach_cell<VERTEX>(map, [&] (Vertex v, unsigned int /*thr*/)
-	{
-		normal[v] = vertexNormal<PFP>(map, v, position) ;
-	}, FORCE_CELL_MARKING);
+    CGoGN::Parallel::foreach_cell<VERTEX>(map, [&] (Vertex v, unsigned int /*thr*/)
+    {
+        normal[v] = vertexNormal<PFP>(map, v, position) ;
+    }, FORCE_CELL_MARKING);
 }
 
 template <typename PFP, typename V_ATT, typename F_ATT>
 void computeNormalFaces(typename PFP::MAP& map, const V_ATT& position, F_ATT& normal)
 {
-	CGoGN::Parallel::foreach_cell<FACE>(map, [&] (Face f, unsigned int /*thr*/)
-	{
-		normal[f] = faceNormal<PFP>(map, f, position) ;
-	});
+    CGoGN::Parallel::foreach_cell<FACE>(map, [&] (Face f, unsigned int /*thr*/)
+    {
+        normal[f] = faceNormal<PFP>(map, f, position) ;
+    });
 }
 
 template <typename PFP, typename V_ATT, typename E_ATT>
 void computeAnglesBetweenNormalsOnEdges(typename PFP::MAP& map, const V_ATT& position, E_ATT& angles)
 {
-	CGoGN::Parallel::foreach_cell<EDGE>(map,[&](Edge e, unsigned int /*thr*/)
-	{
-		angles[e] = computeAngleBetweenNormalsOnEdge<PFP>(map, e, position) ;
-	});
+    CGoGN::Parallel::foreach_cell<EDGE>(map,[&](Edge e, unsigned int /*thr*/)
+    {
+        angles[e] = computeAngleBetweenNormalsOnEdge<PFP>(map, e, position) ;
+    });
 }
 
 } // namespace Parallel
